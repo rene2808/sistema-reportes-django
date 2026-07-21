213670cc2f33c97b0aff6d1ef8117c04fca579b4
rene2808 <renesalas2808@gmail.com>
2026-07-20 20:20:34 -0600

    Agrega script para crear admin

--- crear_admin.py
+++ crear_admin.py
@@ -5,14 +5,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_reportes.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
+from reportes.models import PerfilUsuario
 
 User = get_user_model()
 username = "admin"
 email = "admin@ejemplo.com"
-password = "admin123!"  # <-- Cambia esto por la contraseña que quieras
+password = "admin1234"
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+    user = User.objects.create_superuser(username=username, email=email, password=password)
     print(">>> SUPERUSUARIO CREADO CON ÉXITO <<<")
 else:
     user = User.objects.get(username=username)
@@ -21,3 +22,9 @@ else:
     user.is_staff = True
     user.save()
     print(">>> CONTRASEÑA DE SUPERUSUARIO ACTUALIZADA CON ÉXITO <<<")
+
+# Asegurar que el PerfilUsuario tenga rol de administrador
+perfil, _ = PerfilUsuario.objects.get_or_create(usuario=user)
+perfil.rol = 'administrador'
+perfil.save()
+print(">>> ROL DE PERFIL CONFIGURADO A 'administrador' <<<")
